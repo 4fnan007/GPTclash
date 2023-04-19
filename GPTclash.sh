2ada068ed36a1c6290c66c799537e2007686f962
Afnan K <63971349+4fnan007@users.noreply.github.com>
2023-04-20 00:12:27 +0700

    Update GPTclash.sh

--- GPTclash.sh
+++ GPTclash.sh
@@ -11,7 +11,6 @@ else
   # gnome-terminal is not available, use mate-terminal instead
   # Open a new terminal and execute the first command
   mate-terminal --title="Chat 1" -e "bash -c 'python3 source/firefox_server.py --port 5001 --profile /tmp/chat1'"
-
   
   mate-terminal --title="Chat 2" -e "bash -c 'python3 source/firefox_server.py --port 5002 --profile /tmp/chat2'"
 fi
