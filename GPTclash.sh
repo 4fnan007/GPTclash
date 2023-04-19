18d64a62cdcd0960f64cd53744177e72bb1ac60d
Afnan K <63971349+4fnan007@users.noreply.github.com>
2023-04-20 00:12:12 +0700

    Update GPTclash.sh

--- GPTclash.sh
+++ GPTclash.sh
@@ -12,7 +12,7 @@ else
   # Open a new terminal and execute the first command
   mate-terminal --title="Chat 1" -e "bash -c 'python3 source/firefox_server.py --port 5001 --profile /tmp/chat1'"
 
-  # Open another terminal and execute the second command
+  
   mate-terminal --title="Chat 2" -e "bash -c 'python3 source/firefox_server.py --port 5002 --profile /tmp/chat2'"
 fi
 python3 source/gpt_autoscript.py && fg
